f4c1c4b6720d0349965f670f35eff0ba7dbdb9c3
ajeje93 <serran93@hotmail.it>
2022-06-18 16:28:11 +0200

    fix: adapt to new speedtest cli version data

--- speedtest.py
+++ speedtest.py
@@ -1,8 +1,8 @@
 import subprocess
-import os 
+import os
 import errno
 import json
-import datetime 
+import datetime
 from pymongo import MongoClient
 import time
 import logging
@@ -11,13 +11,14 @@ from timeout import timeout
 
 logging_level = logging.DEBUG
 
+
 def get_module_logger(mod_name):
-    """
+    '''
     To use this, do logger = get_module_logger(__name__)
-    """
+    '''
     global logging_level
     logger = logging.getLogger(mod_name)
-        # reset the logger.handlers if it already exists.
+    # reset the logger.handlers if it already exists.
     if logger.handlers:
         logger.handlers = []
     handler = logging.StreamHandler()
@@ -28,35 +29,41 @@ def get_module_logger(mod_name):
     logger.setLevel(logging_level)
     return logger
 
+
 def set_global_logging_level(logging_level_string):
     global logging_level
-    switcher={
-                'DEBUG': logging.DEBUG,
-                'ERROR': logging.ERROR,
-                'INFO' : logging.INFO,
-                'WARNING' : logging.WARNING,
-                'CRITICAL' : logging.CRITICAL
-             }
-    logging_level = switcher.get(logging_level_string,logging.DEBUG)
+    switcher = {
+        'DEBUG': logging.DEBUG,
+        'ERROR': logging.ERROR,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'CRITICAL': logging.CRITICAL
+    }
+    logging_level = switcher.get(logging_level_string, logging.DEBUG)
+
 
 @timeout(120, os.strerror(errno.ETIMEDOUT))
 def speedtest(mongo_uri, database, collection, speedtest_server_id):
     try:
-        get_module_logger(__name__).info("Performing speedtest...")
+        get_module_logger(__name__).info('Performing speedtest...')
 
         # get speedtest
-        command = ["speedtest-cli", "--json"]
-        if speedtest_server_id != "":
-            command.append("--server")
+        command = ['speedtest-cli', '--json']
+        if speedtest_server_id != '':
+            command.append('--server')
             command.append(speedtest_server_id)
-        
-        get_module_logger(__name__).info("Running command: {0}".format(" ".join(command)))
 
-        result = json.loads(subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        get_module_logger(__name__).debug("speedtest result: {0}".format(result))
+        get_module_logger(__name__).info(
+            'Running command: {0}'.format(' '.join(command)))
+
+        result = json.loads(subprocess.run(
+            command, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+        get_module_logger(__name__).debug(
+            'speedtest result: {0}'.format(result))
 
         # convert timestamp
-        result['timestamp'] = datetime.datetime.strptime(result['timestamp'], "%Y-%m-%dT%H:%M:%S%z")
+        result['timestamp'] = datetime.datetime.strptime(
+            result['timestamp'], '%Y-%m-%dT%H:%M:%S.%f%z')
 
         # insert object in db
         client = MongoClient(mongo_uri)
@@ -64,12 +71,15 @@ def speedtest(mongo_uri, database, collection, speedtest_server_id):
         collection = db[collection]
         object_id = collection.insert_one(result).inserted_id
         client.close()
-        get_module_logger(__name__).debug("object id: {0}".format(object_id))
-        get_module_logger(__name__).debug("inserted obj: {0}".format(collection.find_one({"_id" : object_id})))
+        get_module_logger(__name__).debug('object id: {0}'.format(object_id))
+        get_module_logger(__name__).debug('inserted obj: {0}'.format(
+            collection.find_one({'_id': object_id})))
 
-        get_module_logger(__name__).info("Speedtest completed!")
+        get_module_logger(__name__).info('Speedtest completed!')
     except Exception as e:
-        get_module_logger(__name__).error("Error in speedtest function: {0}".format(e))
+        get_module_logger(__name__).error(
+            'Error in speedtest function: {0}'.format(e))
+
 
 def create_collections(mongo_uri, database, collection):
     try:
@@ -77,80 +87,97 @@ def create_collections(mongo_uri, database, collection):
         client = MongoClient(mongo_uri)
         db = client[database]
 
-        filter = {"name": {"$regex": rf"^(?!system\.)\b(\w*{collection}\w*)\b"}}
+        filter = {
+            'name': {'$regex': rf'^(?!system\.)\b(\w*{collection}\w*)\b'}}
         collection_list = db.list_collection_names(filter=filter)
-        get_module_logger(__name__).debug("Collection list filtered: {0}".format(collection_list))
+        get_module_logger(__name__).debug(
+            'Collection list filtered: {0}'.format(collection_list))
 
-        if collection not in collection_list :
+        if collection not in collection_list:
             db.command('create', collection)
-            get_module_logger(__name__).info("Created collection {0}".format(collection))
+            get_module_logger(__name__).info(
+                'Created collection {0}'.format(collection))
         else:
-            get_module_logger(__name__).debug("Collection {0} already exists".format(collection))
+            get_module_logger(__name__).debug(
+                'Collection {0} already exists'.format(collection))
 
-        if 'normalized_' + collection not in collection_list :
-            pipeline=[{"$project": {
-                        "ts": '$timestamp',
-                        "downloadMbps": {
-                            "$divide": [
-                            "$download.bandwidth",
-                            125000
+        if 'normalized_' + collection not in collection_list:
+            pipeline = [
+                {
+                    '$project': {
+                        'ts': '$timestamp',
+                        'downloadMbps': {
+                            '$divide': [
+                                '$download', 1000000
                             ]
                         },
-                        "uploadMbps": {
-                            "$divide": [
-                            "$upload.bandwidth",
-                            125000
+                        'uploadMbps': {
+                            '$divide': [
+                                '$upload', 1000000
                             ]
                         },
-                        "pingJitter": '$ping.jitter',
-                        "pingLatency": '$ping.latency',
-                        "packetLoss": 1
-                    }}]
-            db.command('create', 'normalized_' + collection, viewOn=collection, pipeline=pipeline)
-            get_module_logger(__name__).info("Created view {0}".format('normalized_' + collection))
+                        'pingLatency': '$ping'
+                    }
+                }
+            ]
+            db.command('create', 'normalized_' + collection,
+                       viewOn=collection, pipeline=pipeline)
+            get_module_logger(__name__).info(
+                'Created view {0}'.format('normalized_' + collection))
         else:
-            get_module_logger(__name__).debug("View {0} already exists".format('normalized_' + collection))
+            get_module_logger(__name__).debug(
+                'View {0} already exists'.format('normalized_' + collection))
 
         client.close()
     except Exception as e:
-        get_module_logger(__name__).error("Error in create_collection function: {0}".format(e))
+        get_module_logger(__name__).error(
+            'Error in create_collection function: {0}'.format(e))
+
 
 def main():
     try:
         load_dotenv()
         # get config main config
-        delay_seconds = int(os.getenv("DELAY_SECONDS", 60))
-        get_module_logger(__name__).debug("Delay between speetests is {0}".format(delay_seconds))
+        delay_seconds = int(os.getenv('DELAY_SECONDS', 60))
+        get_module_logger(__name__).debug(
+            'Delay between speetests is {0}'.format(delay_seconds))
 
-        logging_level_string = os.getenv("LOGGING_LEVEL", "DEBUG")
-        get_module_logger(__name__).debug("Logging level is {0}".format(logging_level_string))
-
+        logging_level_string = os.getenv('LOGGING_LEVEL', 'DEBUG')
+        get_module_logger(__name__).debug(
+            'Logging level is {0}'.format(logging_level_string))
 
         # specific mongo config
-        mongo_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-        get_module_logger(__name__).debug("MongoDB URI is {0}".format(mongo_uri))
+        mongo_uri = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+        get_module_logger(__name__).debug(
+            'MongoDB URI is {0}'.format(mongo_uri))
 
-        database = os.getenv("MONGODB_DB", "network_monitoring")
-        get_module_logger(__name__).debug("MongoDB DB is {0}".format(database))
+        database = os.getenv('MONGODB_DB', 'network_monitoring')
+        get_module_logger(__name__).debug('MongoDB DB is {0}'.format(database))
 
-        collection = os.getenv("MONGODB_COLLECTION", "speedtest")
-        get_module_logger(__name__).debug("MongoDB collection is {0}".format(collection))
+        collection = os.getenv('MONGODB_COLLECTION', 'speedtest')
+        get_module_logger(__name__).debug(
+            'MongoDB collection is {0}'.format(collection))
 
         # speedtest config
-        speedtest_server_id = os.getenv("SPEEDTEST_SERVER_ID", "")
-        get_module_logger(__name__).debug("Speedtest server id is {0}".format(speedtest_server_id))
-        
+        speedtest_server_id = os.getenv('SPEEDTEST_SERVER_ID', '')
+        get_module_logger(__name__).debug(
+            'Speedtest server id is {0}'.format(speedtest_server_id))
+
         # set logging level
         set_global_logging_level(logging_level_string)
 
-        starttime=time.time()
+        starttime = time.time()
         while True:
-            time.sleep(delay_seconds - ((time.time() - starttime) % delay_seconds))
-            get_module_logger(__name__).debug("Waiting for {0} seconds".format(delay_seconds))
+            time.sleep(delay_seconds -
+                       ((time.time() - starttime) % delay_seconds))
+            get_module_logger(__name__).debug(
+                'Waiting for {0} seconds'.format(delay_seconds))
             create_collections(mongo_uri, database, collection)
             speedtest(mongo_uri, database, collection, speedtest_server_id)
     except Exception as e:
-        get_module_logger(__name__).error("Error in main function: {0}".format(e))
+        get_module_logger(__name__).error(
+            'Error in main function: {0}'.format(e))
+
 
 if __name__ == '__main__':
     main()
